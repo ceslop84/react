68fdd4fedd20a41a4c76c5067f3145184d77a784
Cesar Rafael Lopes <cesar.lopes@serpro.gov.br>
2025-02-13 13:25:40 -0300

    Aula 24.

--- react-basico/src/App.js
+++ react-basico/src/App.js
@@ -18,6 +18,27 @@ class App extends Component {
         mensagem: "Olá, eu sou a Maria.",
       },
     ],
+
+    novoComentario: {
+      nome: "",
+      email: "",
+      mensagem: "",
+    },
+  };
+
+  adicionarComentario = (evento) => {
+    evento.preventDefault();
+    console.log("Adicionando comentário");
+    const novoComentario = {...this.state.novoComentario, data: new Date()};
+    this.setState({
+      comentarios: [...this.state.comentarios, novoComentario],
+      novoComentario: { nome: "", email: "", mensagem: "" },
+    });
+  };
+
+  digitacao = (evento) => {
+    const { name, value } = evento.target;
+    this.setState({ novoComentario: { ...this.state.novoComentario, [name]: value } });
   };
 
   render() {
@@ -26,14 +47,36 @@ class App extends Component {
         <h1>Meu projeto</h1>
 
         {this.state.comentarios.map((comentario, indice) => (
-          <Comentario 
-          key={indice}
-          nome={comentario.nome} 
-          email={comentario.email}
-          data={comentario.data}>
-          {comentario.mensagem}
+          <Comentario key={indice} nome={comentario.nome} email={comentario.email} data={comentario.data}>
+            {comentario.mensagem}
           </Comentario>
         ))}
+
+        <form method="post" onSubmit={this.adicionarComentario}>
+          <h2>Adicionar comentário</h2>
+          <div>
+            <input
+              type="text"
+              name="nome"
+              placeholder="Digite seu nome"
+              value={this.state.novoComentario.nome}
+              onChange={this.digitacao}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="email"
+              placeholder="Digite seu e-mail"
+              value={this.state.novoComentario.email}
+              onChange={this.digitacao}
+            />
+          </div>
+          <div>
+            <textarea name="mensagem" value={this.state.novoComentario.mensagem} rows="4" onChange={this.digitacao} />
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
       </div>
     );
   }
